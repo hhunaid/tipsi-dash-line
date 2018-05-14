fedfe220a135df2a7715dd30ebe6448d51437a46
Hunaid Hassan <hhunaid@gmail.com>
2018-05-15 00:04:22 +0500

    feat: Added support for RN 0.49+

--- src/DashLine.js
+++ src/DashLine.js
@@ -1,9 +1,10 @@
-import React, { Component, PropTypes } from 'react'
-import { View, requireNativeComponent } from 'react-native'
+import React, { Component } from 'react'
+import { View, requireNativeComponent, ViewPropTypes } from 'react-native'
+import PropTypes from 'prop-types';
 
 export default class DashLine extends Component {
   static propTypes = {
-    ...View.propTypes,
+    ...ViewPropTypes,
     dashColor: PropTypes.string,
     dashLineWidth: PropTypes.number,
     dashLineSpace: PropTypes.number,
